aea9c8434808fd230bf2d0a2d635b79d2e2d0ba9
lw <lw@DESKTOP-0ON3BNM>
2017-11-14 10:12:56 +0800

    17-11-14

--- ToMysql.py
+++ ToMysql.py
@@ -5,7 +5,7 @@ import pymysql
 
 def query_mysql(sql): # query SQL语句
     # 打开数据库连接
-    db = pymysql.connect("60.30.156.148", "root", "bairiyishanjin", "app_adm", charset="utf8")
+    db = pymysql.connect("60.30.156.148", "usr", "pws", "app_adm", charset="utf8")
     # 使用 cursor() 方法创建一个游标对象 cursor
     # cursor = db.cursor()
     cursor = db.cursor(pymysql.cursors.DictCursor)
